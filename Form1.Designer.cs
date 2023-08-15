749520fb9c5b0f8b1c7171793f7d0a59d4ec725e
bhs <bhs@01PORTARIA>
2023-08-15 01:20:42 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,259 @@
+﻿namespace CRUDcsv
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnEditar = new System.Windows.Forms.Button();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnLancar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Cyan;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(358, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 25);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "LIMPAR";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.Color.Cyan;
+            this.btnExcluir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnExcluir.Location = new System.Drawing.Point(358, 173);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(138, 25);
+            this.btnExcluir.TabIndex = 7;
+            this.btnExcluir.Text = "EXCLUIR";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnEditar
+            // 
+            this.btnEditar.BackColor = System.Drawing.Color.Cyan;
+            this.btnEditar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEditar.Location = new System.Drawing.Point(358, 137);
+            this.btnEditar.Name = "btnEditar";
+            this.btnEditar.Size = new System.Drawing.Size(138, 25);
+            this.btnEditar.TabIndex = 8;
+            this.btnEditar.Text = "EDITAR";
+            this.btnEditar.UseVisualStyleBackColor = false;
+            this.btnEditar.Click += new System.EventHandler(this.btnEditar_Click);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.BackColor = System.Drawing.Color.Cyan;
+            this.btnPesquisar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPesquisar.Location = new System.Drawing.Point(358, 101);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(138, 25);
+            this.btnPesquisar.TabIndex = 9;
+            this.btnPesquisar.Text = "PESQUISAR";
+            this.btnPesquisar.UseVisualStyleBackColor = false;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnLancar
+            // 
+            this.btnLancar.BackColor = System.Drawing.Color.Cyan;
+            this.btnLancar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLancar.Location = new System.Drawing.Point(358, 66);
+            this.btnLancar.Name = "btnLancar";
+            this.btnLancar.Size = new System.Drawing.Size(138, 25);
+            this.btnLancar.TabIndex = 10;
+            this.btnLancar.Text = "LANÇAR";
+            this.btnLancar.UseVisualStyleBackColor = false;
+            this.btnLancar.Click += new System.EventHandler(this.btnLancar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtId);
+            this.groupBox1.Controls.Add(this.txtEmail);
+            this.groupBox1.Controls.Add(this.txtNome);
+            this.groupBox1.Location = new System.Drawing.Point(37, 64);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(257, 166);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Cyan;
+            this.label4.Location = new System.Drawing.Point(13, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Cyan;
+            this.label3.Location = new System.Drawing.Point(13, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "E-MAIL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Cyan;
+            this.label2.Location = new System.Drawing.Point(13, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "NOME:";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtId.Location = new System.Drawing.Point(10, 40);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(48, 22);
+            this.txtId.TabIndex = 1;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(10, 124);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(235, 22);
+            this.txtEmail.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(10, 81);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(235, 22);
+            this.txtNome.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Cyan;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSair);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(539, 44);
+            this.panel1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.label1.Location = new System.Drawing.Point(32, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CRUD CSV";
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.Cyan;
+            this.btnSair.BackgroundImage = global::CRUDcsv.Properties.Resources._9004715_cross_delete_remove_cancel_icon;
+            this.btnSair.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnSair.FlatAppearance.BorderSize = 0;
+            this.btnSair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSair.Location = new System.Drawing.Point(501, 2);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(37, 25);
+            this.btnSair.TabIndex = 3;
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(539, 251);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnEditar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.btnLancar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnEditar;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnLancar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CRUDcsv
+{
+    public partial class Form1 : Form
+    {
+        public static String CRUDcsv = Directory.GetCurrentDirectory() + "\\CRUDcsv.csv";
+
+        public string BUSCA, ID, NOME, EMAIL, TUDO;
+        int LINHA = 0;
+
+        public Random random;
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtId.Text))
+                {
+                    MessageBox.Show("Forneça o ID");
+                }
+                else
+                {
+                    if (LINHA > 0)
+                    {
+                        bool linhaDeletada = DeletarLinhaCSV(CRUDcsv, LINHA);
+                        if (linhaDeletada)
+                        {         
+                            txtId.Clear();
+                            txtNome.Clear();
+                            txtEmail.Clear();
+
+                            int numeroAleatorio = random.Next(1, 10001);
+                            txtId.Text = numeroAleatorio.ToString();
+
+                            MessageBox.Show("Registro deletado com sucesso !");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Não há registro ativo para esse ID");
+                        }
+                    }
+                }
+            }
+            catch (Exception EX)
+            {
+
+                MessageBox.Show("ERRO: " + EX.Message);
+            }
+            
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtId.Text))
+                {
+                    MessageBox.Show("Forneça o ID");
+                }
+                else
+                {
+                    LINHA = BuscarLinhaCSV(CRUDcsv, txtId.Text);
+
+                    BUSCA = Convert.ToString(txtId.Text);
+
+                    string[] lines = File.ReadAllLines(CRUDcsv);
+
+                    using (StreamReader reader = new StreamReader(CRUDcsv))
+                    {
+                        foreach (string line in lines)
+                        {
+                            string[] values = line.Split(';');
+
+                            if (values.Contains(BUSCA))
+                            {
+                                txtId.Text = values[0];
+                                txtNome.Text = values[1];
+                                txtEmail.Text = values[2];
+                                return;
+                            }
+                        }
+                    }
+
+                    MessageBox.Show("Pesquisa não encontrada");
+                }
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("ERRO: " + EX.Message);
+            }
+        }
+
+        private void btnLancar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtId.Text))
+                {
+                    MessageBox.Show("Forneça o ID");
+                }
+                else
+                {
+                    if (File.Exists(CRUDcsv))
+                    {
+
+                        ID = txtId.Text;
+                        NOME = txtNome.Text;
+                        EMAIL = txtEmail.Text;
+
+                        TUDO = ID + ";" + NOME + ";" + EMAIL;
+
+                        string[] DadosCompletos = { ID, NOME, EMAIL };
+                        using (StreamWriter SW = new StreamWriter(CRUDcsv, true))
+                        {
+                            SW.WriteLine(TUDO);
+                        }
+
+                        txtId.Clear();
+                        txtNome.Clear();
+                        txtEmail.Clear();
+
+                        int numeroAleatorio = random.Next(1, 10001);
+                        txtId.Text = numeroAleatorio.ToString();
+
+                        MessageBox.Show("Entrada efetuada com sucesso !");
+                    }
+                }
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("ERRO :" + EX.Message);
+            }            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtId.Clear();
+            txtNome.Clear();
+            txtEmail.Clear();
+
+            int numeroAleatorio = random.Next(1, 10001);
+            txtId.Text = numeroAleatorio.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int numeroAleatorio = random.Next(1, 10001);
+            txtId.Text = numeroAleatorio.ToString();
+        }
+
+        private void btnEditar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(txtId.Text))
+                {
+                    MessageBox.Show("Forneça o ID");
+                }
+                else
+                {
+                    LINHA = BuscarLinhaCSV(CRUDcsv, txtId.Text);
+
+                    if (LINHA != -1)
+                    {
+                        string[] lines = File.ReadAllLines(CRUDcsv);
+                        lines[LINHA - 1] = txtId.Text + ";" + txtNome.Text + ";" + txtEmail.Text;
+
+                        File.WriteAllLines(CRUDcsv, lines);
+
+                        MessageBox.Show("Registro editado com sucesso !");
+
+                        txtId.Clear();
+                        txtNome.Clear();
+                        txtEmail.Clear();
+
+                        int numeroAleatorio = random.Next(1, 10001);
+                        txtId.Text = numeroAleatorio.ToString();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Registro não encontrado");
+                    }
+                }
+            }
+            catch (Exception EX)
+            {
+                MessageBox.Show("ERRO: " + EX.Message);
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            random = new Random();
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Deseja sair ?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private bool DeletarLinhaCSV(string pathArquivoCSV, int numeroLinha)
+        {
+            string arquivoTemporario = Path.GetTempFileName();
+
+            using (StreamReader reader = new StreamReader(CRUDcsv))
+            using (StreamWriter writer = new StreamWriter(arquivoTemporario))
+            {
+                string linha;
+                int linhaAtual = 1;
+
+                while ((linha = reader.ReadLine()) != null)
+                {
+                    if (linhaAtual != numeroLinha)
+                    {
+                        writer.WriteLine(linha);
+                    }
+
+                    linhaAtual++;
+                }
+            }
+
+            File.Delete(CRUDcsv);
+            File.Move(arquivoTemporario, CRUDcsv);
+
+            return File.Exists(CRUDcsv);
+        }
+
+        private int BuscarLinhaCSV(string pathArquivoCSV, string valorBusca)
+        {
+            using (StreamReader reader = new StreamReader(CRUDcsv))
+            {
+                string linha;
+                int numeroLinha = 0;
+
+                while ((linha = reader.ReadLine()) != null)
+                {
+                    numeroLinha++;
+
+                    // Verifica se a linha contém o valor de busca
+                    if (linha.Contains(valorBusca))
+                    {
+                        return numeroLinha;
+                    }
+                }
+            }
+
+            return -1; // Valor não encontrado
+        }
+    }
+}
